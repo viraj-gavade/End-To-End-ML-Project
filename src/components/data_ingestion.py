374afbebe7ceed60ce55a3e3675f0cb2fa656a20
Viraj Gavade <vrajgavade17@gmail.com>
2025-06-09 13:02:13 +0530

    Data Injetion is done

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,58 @@
+import os 
+import sys
+from src.exception_handler import CustomException
+from src.logger import logging
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+
+@dataclass
+class DataInjetionConfig:
+    train_data_path : str = os.path.join('artifact','train.csv')
+    test_data_path : str = os.path.join('artifact','test.csv')
+    raw_data_path : str = os.path.join('artifact','raw.csv')
+
+
+class DataInjetion:
+    def __init__(self):
+        self.injetion_config = DataInjetionConfig()
+
+    
+    def initiate_data_ingestion(self):
+        logging.info('Entered the data injetion method ')
+        try:
+            df = pd.read_csv('notebook/data/stud.csv')
+            logging.info('Read the data set as dataframe')
+
+            os.makedirs(os.path.dirname(self.injetion_config.train_data_path),exist_ok=True)
+
+            df.to_csv(self.injetion_config.raw_data_path,index=False,header=True)
+            logging.info('Trian test split is initiated')
+
+            train_set,test_set = train_test_split(df,test_size=0.2,random_state=42)
+
+            train_set.to_csv(self.injetion_config.train_data_path,index=False,header=True)
+
+            test_set.to_csv(self.injetion_config.test_data_path,index=False,header=True)
+
+
+            logging.info('Injection of the data is completed!')
+
+            return(
+                self.injetion_config.train_data_path,
+                self.injetion_config.test_data_path,
+
+            )
+
+        except Exception as e:
+            raise CustomException(e,sys)
+            
+
+
+
+if __name__ == '__main__':
+
+    obj=DataInjetion()
+    obj.initiate_data_ingestion()
+        
